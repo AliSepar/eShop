0b24348b493cb5b62921a9721fc408727a69b0ea
AliSepar <alisabah.separ1@gmail.com>
2024-09-24 16:08:40 +0200

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/ui/Footer";
+import Navbar from "./components/ui/Navbar";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline bg-red-300">Hello world!</h1>
+
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
